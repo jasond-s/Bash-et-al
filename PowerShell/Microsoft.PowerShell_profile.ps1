f3552d8ff51341cf15c4892a02c0efe39992f795
Jason Dryhurst-Smith <jason@jasonds.co.uk>
2020-06-05 14:10:00 +0100

    bash

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,53 +1,17 @@
 ﻿
-# Load posh-git example profile
-#. 'C:\Users\jdryh\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
-
-#First in your powershell profile in
-#C:\Users\<<username>>\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
- 
-#Then in:
-#C:\Users\<<username>>\Documents\WindowsPowerShell\Get-ChildItemColor.ps1
-
-# function prompt {
-#     "$(split-path "$pwd" -leaf -resolve) $lastexitcode $Global:GitStatus #>"
-# } 
+Import-Module go
+Import-Module posh-git
 
 Set-PSReadlineOption -BellStyle None
 
-try {
-    Import-Module posh-git
-}
-catch [System.Exception] {
-    cd $env:temp
-    git clone https://github.com/dahlbyk/posh-git.git    
-    .\install.ps1
-
-    Import-Module posh-git
-}
-
-# Set up a simple prompt, adding the git prompt parts inside git repos
 function global:prompt {
     $realLASTEXITCODE = $LASTEXITCODE
 
-    # Reset color, which can be messed up by Enable-GitColors
-    $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
-
-    Write-Host($(split-path "$pwd" -leaf -resolve)) -nonewline
-
-    $fore = $Host.UI.RawUI.ForegroundColor
-    if (get-job){        
-        $Host.UI.RawUI.ForegroundColor = 'Green'
-        Write-Host " (⌐■_■)" -nonewline
-    } else {        
-        $Host.UI.RawUI.ForegroundColor = 'Cyan'
-        Write-Host " ( •_•)" -nonewline
-    }
-    $Host.UI.RawUI.ForegroundColor = $fore
-
     Write-VcsStatus
 
+    Write-Output(" " + $(split-path "$pwd" -leaf -resolve) + " #> ") -nonewline
+    
     $global:LASTEXITCODE = $realLASTEXITCODE
-    return " #> "
 }
 
 Enable-GitColors
@@ -55,25 +19,6 @@ Enable-GitColors
 Pop-Location
 
 function Get-ChildItemColor {
-    <#
-    .Synopsis
-    Returns childitems with colors by type.
-    From http://poshcode.org/?show=878
-    .Description
-    This function wraps Get-ChildItem and tries to output the results
-    color-coded by type:
-    Compressed - Yellow
-    Directories - Dark Cyan
-    Executables - Green
-    Text Files - Cyan
-    Others - Default
-    .ReturnValue
-    All objects returned by Get-ChildItem are passed down the pipeline
-    unmodified.
-    .Notes
-    NAME: Get-ChildItemColor
-    AUTHOR: Tojo2000 <tojo2000@tojo2000.com>
-    #>
     $regex_opts = ([System.Text.RegularExpressions.RegexOptions]::IgnoreCase `
     -bor [System.Text.RegularExpressions.RegexOptions]::Compiled)
      
@@ -85,7 +30,7 @@ function Get-ChildItemColor {
     $text_files = New-Object System.Text.RegularExpressions.Regex('\.(txt|cfg|conf|ini|csv|log)$', $regex_opts)
      
     Invoke-Expression ("Get-ChildItem $args") |
-    %{
+    ForEach-Object{
         $c = $fore
         if ($_.GetType().Name -eq 'DirectoryInfo') {
             $c = 'DarkCyan'
@@ -101,14 +46,16 @@ function Get-ChildItemColor {
             $c = 'Yellow'
         }
         $Host.UI.RawUI.ForegroundColor = $c
-        echo $_
+        Write-Output $_
         $Host.UI.RawUI.ForegroundColor = $fore
     }
 }
 
-# set-location D:\code
-# . "C:\Users\<<username>>\Documents\WindowsPowerShell\Get-ChildItemColor.ps1" # read the colourized ls
 set-alias ls Get-ChildItemColor -force -option allscope
 function Get-ChildItem-Force { ls -Force }
 set-alias la Get-ChildItem-Force -option allscope
- 
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
